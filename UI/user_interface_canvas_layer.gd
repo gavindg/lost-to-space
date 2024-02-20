21981a63a461c7386ac821b589ec026343e0b4eb
Weixiao Liang <ChrisLiangwx@gmail.com>
2024-02-20 14:53:12 -0800

    add game timer
    add a timer to count play time; change the style of health bar

--- UI/user_interface_canvas_layer.gd
+++ UI/user_interface_canvas_layer.gd
@@ -1,11 +1,16 @@
 extends CanvasLayer
 
 
+@onready var health_bar:ProgressBar = $health_ProgressBar
+@onready var timer = $Timer
+
+var time_passed = 0;
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	timer.start()
 	
-@onready var health_bar:ProgressBar = $health_ProgressBar
+
 
 func update_health_bar():
 	health_bar.value = Globals.player_health
@@ -14,3 +19,12 @@ func update_health_bar():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+
+
+func _on_timer_timeout():
+	time_passed += 1
+	var hours = time_passed / 1.0 / 3600
+	var minutes = (time_passed % 3600) / 1.0 / 60
+	var seconds = time_passed % 60
+	$time_Label.text = "%02d:%02d:%02d" % [hours, minutes, seconds]
