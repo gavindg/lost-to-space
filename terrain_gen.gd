3fb1d6662d0a407612b05a5bc996f3a7898fce18
Lucas Zhong <zhonglucas@gmail.com>
2024-02-21 19:40:49 -0800

    Add sedcond noise map for more caves, and also change overall placement of dirt

--- terrain_gen.gd
+++ terrain_gen.gd
@@ -14,10 +14,19 @@ var altitude_noise_layer = {}
 
 @onready var tilemap = $TileMap
 
-@export var noise_height_text : NoiseTexture2D
+const GRASS = Vector2i(0, 6)
+const BLACKNESS = Vector2i(0, 12)
 
 func gen_noise_map(frequency, octaves, lacunarity, gain, width, height):
 	var noise_map = FastNoiseLite.new()
+	var second_map = FastNoiseLite.new()
+	
+	second_map.seed = randi()
+	second_map.noise_type = FastNoiseLite.TYPE_SIMPLEX_SMOOTH
+	second_map.frequency = frequency
+	second_map.fractal_octaves = octaves
+	second_map.fractal_lacunarity = lacunarity
+	second_map.fractal_gain = gain
 	
 	noise_map.seed = randi()	
 	noise_map.noise_type = FastNoiseLite.TYPE_PERLIN
@@ -30,37 +39,34 @@ func gen_noise_map(frequency, octaves, lacunarity, gain, width, height):
 	
 	noise_map.frequency = 0.01
 	
-	var maxbruh = 0
-	var minbruh = 1203810293812
 	for x in range(-width, width):
-		for y in range(-height, height):
-			var noise = noise_map.get_noise_2d(x,y)
+		var ground = abs(noise_map.get_noise_2d(x,0) * 60)
+		for y in range(ground, height):
+			var noise = min(noise_map.get_noise_2d(x,y), second_map.get_noise_2d(x,y))
 			#var noise = abs(noise_map.get_noise_2d(x,y) * 2 - 1)
 			grid[Vector2i(x,y)] = noise
-			minbruh = min(noise, minbruh)
-			maxbruh = max(noise, maxbruh)
-	print(maxbruh)
-	print(minbruh)
+			if noise > -0.25:
+				tilemap.set_cell(0, Vector2i(x,y), 1, GRASS)
 	return grid
 	
 func gen_normalized_terrain(width, height):
 	for x in range(-width, width):
-		for y in range(-height, height):
+		for y in range(height):
 			var pos = Vector2i(x, y)
 			var alt = altitude_noise_layer[pos]
 			
-			if alt < 0: 
-				tilemap.set_cell(0, pos, 1, Vector2i(0,0))
-			elif alt > 0 and alt < 1.0:
-				tilemap.set_cell(0, pos, 1, Vector2i(0,6))
-			else:  
-				tilemap.set_cell(0, pos, 1, Vector2i(0,12))
+			if alt > -0.1: 
+				tilemap.set_cell(0, pos, 1, GRASS)
+			#elif alt > 0 and alt < 1.0:
+			#	tilemap.set_cell(0, pos, 1, Vector2i(0,6))
+			else:
+				tilemap.set_cell(0, pos, 1, BLACKNESS)
 	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	altitude_noise_layer = gen_noise_map(alt_freq, oct, lac, gain, map_width, map_height)
-	gen_normalized_terrain(map_width, map_height)
+	#gen_normalized_terrain(map_width, map_height)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
