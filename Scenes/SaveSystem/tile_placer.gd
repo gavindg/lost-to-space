bbe9fbe01d122079ab5a47074c0382aab73bc0ea
gavindg <52291693+gavindg@users.noreply.github.com>
2024-02-22 20:12:07 -0800

    Update tile_placer.gd

--- Scenes/SaveSystem/tile_placer.gd
+++ Scenes/SaveSystem/tile_placer.gd
@@ -15,12 +15,13 @@ func _unhandled_input(event: InputEvent) -> void:
 	# on click
 	if event is InputEventMouseButton:
 		if world == null || player == null:
-			print("[TilePlacer]: nothing to do; tilemap is ", world, "player is ", player)
+			if debug_messages:
+				print("[TilePlacer]: nothing to do; tilemap is ", world, "player is ", player)
 			return
 		
 		# first check if click was within the player's placement radius
-		var click_pos : Vector2 = event.global_position
-		if click_pos.distance_to(player.global_position) < player_block_placement_radius:
+		var click_pos : Vector2 = event.global_position  # get click position
+		if in_range(click_pos):
 			# convert global click coords to map coords
 			var coords = world.local_to_map(world.to_local(click_pos))
 			
@@ -37,3 +38,7 @@ func _unhandled_input(event: InputEvent) -> void:
 			print("click (too far) @", event.position)
 			print("player @", player.global_position)
 			print("distance:", click_pos.distance_to(player.global_position))
+
+
+func in_range(click_pos) -> bool:
+	return click_pos.distance_to(player.global_position) < player_block_placement_radius;
