98c420294dc804f950714611c867600b274cbac1
Lucas Zhong <zhonglucas@gmail.com>
2024-03-08 17:56:19 -0800

    GEnerate background walls in another layer

--- Modules/WorldGen/terrain_gen.gd
+++ Modules/WorldGen/terrain_gen.gd
@@ -22,10 +22,13 @@ var spawn_location
 const ABOVE_GROUND = -10
 const GRASS_LEVEL = -20
 
+const FOREGROUND = 1
+const BACKGROUND = 0
+
 const GRASS = Vector2i(0,0)
-const GRASS_TOP = Vector2i(4,0)
-const STONE = Vector2i(0,0)
-const BLACKNESS = Vector2i(0, 24)
+const GRASS_TOP = Vector2i(2,0)
+const STONE = Vector2i(0,1)
+const BLACKNESS = Vector2i(0,6)
 const RED = Vector2i(24,12)
 
 func gen_new_noise(noise_type, freq, oct, lac, g):
@@ -56,9 +59,13 @@ func gen_noise_map():
 				continue
 			#var noise = primary_noise.get_noise_2d(x,y)
 			#var noise = min(primary_noise.get_noise_2d(x,y), second_noise.get_noise_2d(x,y)) # Take the minimum of two noises	
-			tilemap.set_cell(0, pos, 3, GRASS)
+			tilemap.set_cell(FOREGROUND, pos, 0, GRASS)
 			if y > ground_level + cave_biome_change:
-				tilemap.set_cell(0, pos, 2, STONE)
+				tilemap.set_cell(FOREGROUND, pos, 0, STONE)
+			elif y > ground_level + cave_biome_change * 2:
+				tilemap.set_cell(FOREGROUND, pos, 0, Vector2i(0,3))
+			elif y > ground_level + cave_biome_change * 3:
+				tilemap.set_cell(FOREGROUND, pos, 0, Vector2i(0,5))
 			else: 
 				grid[pos] = GRASS_LEVEL
 	return grid	
@@ -70,7 +77,7 @@ func gen_caves():
 		for y in range(ground_levels[x] + cave_offset + 1, map_height):
 			if cave_noise.get_noise_2d(x,y) < noise_threshold:
 				if secondary_noise.get_noise_2d(x,y) < 0.35:
-					tilemap.set_cell(0, Vector2i(x,y), 1, BLACKNESS)
+					tilemap.set_cell(FOREGROUND, Vector2i(x,y), 0, BLACKNESS)
 					noise_grid[Vector2i(x,y)] = 12390
 
 func gen_grass():
@@ -78,7 +85,7 @@ func gen_grass():
 		var ground = ground_levels[i] + 1
 		var pos = Vector2i(i, ground)
 		if noise_grid[pos] == GRASS_LEVEL:
-			tilemap.set_cell(0, pos, 3, GRASS_TOP)
+			tilemap.set_cell(FOREGROUND, pos, 0, GRASS_TOP)
 
 func gen_spawn_area():
 	var sel 
@@ -95,8 +102,12 @@ func gen_ore():
 	for x in range(-map_width, map_width):
 		for y in range(ground_levels[x] + cave_offset +1, map_height):
 			if ore_noise.get_noise_2d(x,y) < ore_rarity:
-				tilemap.set_cell(0, Vector2i(x,y), 2, Vector2i(2,1))
+				tilemap.set_cell(FOREGROUND, Vector2i(x,y), 0, Vector2i(2,1))
 
+func gen_walls():
+	for x in range(-map_width, map_width):
+		for y in range(ground_levels[x] + cave_biome_change, map_height):
+			tilemap.set_cell(BACKGROUND, Vector2i(x,y), 0, BLACKNESS)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -106,8 +117,9 @@ func _ready():
 	gen_caves()
 	gen_spawn_area()
 	gen_grass()
+	gen_walls()
 	
-	tilemap.set_cell(0, Vector2i(0,0), 1, Vector2i(2,6))
+	tilemap.set_cell(FOREGROUND, Vector2i(0,0), 1, Vector2i(2,6))
 	#gen_normalized_terrain(map_width, map_height)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
