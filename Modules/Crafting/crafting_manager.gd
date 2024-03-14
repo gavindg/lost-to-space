aabf2fdc8b309ebc51b9f569cce6a950c476774f
Erick Guerrero <guerree9@uci.edu>
2024-03-13 18:06:29 -0700

    Toggle crafting ui

--- Modules/Crafting/crafting_manager.gd
+++ Modules/Crafting/crafting_manager.gd
@@ -3,15 +3,24 @@ extends Control
 @export var player: CharacterBody2D
 
 @onready var craft_ui = $Crafting_UI
+@onready var is_open = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	craft_ui.create_item_arrays()
 	craft_ui.set_crafting(player.inv)
+	visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	check_toggle_craft()
 
-
+func check_toggle_craft():
+	if Input.is_action_just_pressed("c"):
+		if is_open:
+			is_open = false
+			visible = false
+		else: 
+			is_open = true
+			visible = true
