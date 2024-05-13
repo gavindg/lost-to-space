333b5bd3f93dcfe0aabe9e5182427093fff8145c
gavindg <52291693+gavindg@users.noreply.github.com>
2024-05-13 12:58:35 -0700

    added text when boss dies

--- testing/winner.gd
+++ testing/winner.gd
@@ -3,14 +3,24 @@ extends Node2D
 # most scuffed script in the universe
 
 @export var audio : AudioStreamPlayer
+@export var text : RichTextLabel
+@export var tween_speed := .5
 
 func _ready():
 	if not audio: 
 		audio = $PVZ
 	
-	if audio:
+	if audio and text:
+		text.modulate.a = 0
 		audio.play()
+		var tween = get_tree().create_tween()
+		await tween.tween_property(text, "modulate:a", 
+									1, tween_speed).finished
+		
 		await audio.finished
+		tween = get_tree().create_tween()
+		await tween.tween_property(text, "modulate:a", 
+									0, tween_speed).finished
 		queue_free()
 	else:
 		print('ERROR: audio not found...')
