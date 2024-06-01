a87eefde529d65da1a774c152079dbb21881cf0d
Robert Gellner <robertmgellner@gmail.com>
2024-06-01 01:46:22 -0700

    added credits

---
+++ Modules/Character/Scenes/Credits Box.gd
@@ -0,0 +1,11 @@
+extends RichTextLabel
+
+
+# Called when the node enters the scene tree for the first time.
+
+func _ready():
+	align = RichTextLabel.ALIGN_CENTER
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
