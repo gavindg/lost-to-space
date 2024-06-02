f819e3d6ca14986fbaa725df84b42c60176eb021
Lucas Zhong <zhonglucas@gmail.com>
2024-06-01 22:59:11 -0700

    Attached credits to end of slimus prime expunged

--- testing/winner.gd
+++ testing/winner.gd
@@ -21,7 +21,13 @@ func _ready():
 		tween = get_tree().create_tween()
 		await tween.tween_property(text, "modulate:a", 
 									0, tween_speed).finished
-		queue_free()
+		#queue_free()
+		go_to_credits()
 	else:
 		print('ERROR: audio not found...')
 		queue_free()
+		
+func go_to_credits():
+	print("called")
+	await get_tree().create_timer(1.0).timeout
+	get_tree().change_scene_to_file("res://Modules/Cutscenes/creditz.tscn")
